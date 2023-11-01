2d7b9b64ca86d4b586253c0ee22360809f7c96ea
jhoID <leress@hotmail.com>
2023-11-01 12:20:57 -0500

    resistor_color

---
+++ lists/resistor_color.py
@@ -0,0 +1,10 @@
+COLORS = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"]
+
+def color_code(color):
+    colors = [color.lower() for color in COLORS  ]
+    return COLORS.index(color)
+
+
+def colors():
+    for color in COLORS:
+        print(color)
