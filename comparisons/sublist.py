ee03842c3b03b64551a2ebd55405a5460deb3de1
jhoID <leress@hotmail.com>
2023-11-08 15:59:34 -0500

    sublist exercise

--- comparisons/sublist.py
+++ comparisons/sublist.py
@@ -1,23 +1,22 @@
-"""
-This exercise stub and the test suite contain several enumerated constants.
-
-Enumerated constants can be done with a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
-efficient.
-It is a common practice to export both constants and functions that work with
-those constants (ex. the constants in the os, subprocess and re modules).
-
-You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
-"""
-
 # Possible sublist categories.
 # Change the values as you see fit.
-SUBLIST = None
-SUPERLIST = None
-EQUAL = None
-UNEQUAL = None
+SUBLIST = 0
+SUPERLIST = 1
+EQUAL = 2
+UNEQUAL = 3
 
 
 def sublist(list_one, list_two):
+    list_one = "".join(str(number) + ", " for number in list_one)
+    list_two = "".join(str(number) + ", " for number in list_two)
+
+    if list_one == list_two:
+        return EQUAL
+
+    if list_two in list_one:
+        return SUPERLIST
+
     if list_one in list_two:
-        print(f'{list_one} is superlist of {list_two}')
+        return SUBLIST
+
+    return UNEQUAL
